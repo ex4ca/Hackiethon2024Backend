11d16d12b3e2e51cf060c31f105b6d80acbf6a4a
kaylynthomson <80387680+kaylynthomson@users.noreply.github.com>
2024-03-24 04:05:17 +1100

    bots

---
+++ Submissions/Bot8.py
@@ -0,0 +1,74 @@
+# bot code goes here
+from Game.Skills import *
+from Game.projectiles import *
+from ScriptingHelp.usefulFunctions import *
+from Game.playerActions import defense_actions, attack_actions, projectile_actions
+from Game.gameSettings import HP, LEFTBORDER, RIGHTBORDER, LEFTSTART, RIGHTSTART, PARRYSTUN
+
+
+# PRIMARY CAN BE: Teleport, Super Saiyan, Meditate, Dash Attack, Uppercut, One Punch
+# SECONDARY CAN BE : Hadoken, Grenade, Boomerang, Bear Trap
+
+# TODO FOR PARTICIPANT: Set primary and secondary skill here
+PRIMARY_SKILL = OnePunchSkill
+SECONDARY_SKILL = SuperArmorSkill
+
+#constants, for easier move return
+#movements
+JUMP = ("move", (0,1))
+FORWARD = ("move", (1,0))
+BACK = ("move", (-1,0))
+JUMP_FORWARD = ("move", (1,1))
+JUMP_BACKWARD = ("move", (-1, 1))
+
+# attacks and block
+LIGHT = ("light",)
+HEAVY = ("heavy",)
+BLOCK = ("block",)
+
+PRIMARY = get_skill(PRIMARY_SKILL)
+SECONDARY = get_skill(SECONDARY_SKILL)
+CANCEL = ("skill_cancel", )
+
+# no move, aka no input
+NOMOVE = "NoMove"
+# for testing
+moves = SECONDARY,
+moves_iter = iter(moves)
+
+# TODO FOR PARTICIPANT: WRITE YOUR WINNING BOT
+class Script:
+    def __init__(self):
+        self.primary = PRIMARY_SKILL
+        self.secondary = SECONDARY_SKILL
+        
+    # DO NOT TOUCH
+    def init_player_skills(self):
+        return self.primary, self.secondary
+
+    # MAIN FUNCTION that returns a single move to the game manager
+    def get_move(self, player, enemy, player_projectiles, enemy_projectiles):
+        # calculate distance between enemy and player
+        distance = abs(get_pos(player)[0] - get_pos(enemy)[0])
+
+        # MAIN FUNCTION that returns a single move to the game manager
+    def get_move(self, player, enemy, player_projectiles, enemy_projectiles):
+        distance = abs(get_pos(player)[0] - get_pos(enemy)[0])
+
+        if distance == 1 and not primary_on_cooldown(player):
+            return PRIMARY
+        elif distance == 1 and primary_on_cooldown(player):
+            if heavy_on_cooldown(player):
+                return LIGHT 
+            return HEAVY   
+
+        if distance > 1 and primary_on_cooldown(player) and get_hp(player) > 30:
+            return FORWARD
+
+        if get_hp(player) < 50 and not secondary_on_cooldown(player):
+            return SECONDARY
+        
+        if not secondary_on_cooldown(enemy):
+            return BLOCK
+
+        return BLOCK 
